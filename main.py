e5a3b3814bb58378aceee5200977bc1e6981a450
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-11-30 13:45:03 -0500

    working on the pie chart about to display count of most A grade restaurant by Borough

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ from restaurant_data import RestaurantData
 from time import sleep
 
 # -------------------------- Initial Set-Up --------------------------
-# tk.Tk().withdraw() # prevents an empty tkinter window from appearing
+tk.Tk().withdraw()  # prevents an empty tkinter window from appearing
 
 # Opening up CSV file choosen by user
 
-# initial_dir = os.path.expanduser("~/Desktop")
-# file_path = filedialog.askopenfilename(initialdir=initial_dir)
-file_path = (
-    "/Users/alehs/Downloads/DOHMH_New_York_City_Restaurant_Inspection_Results.csv"
-)
+initial_dir = os.path.expanduser("~/Desktop")
+file_path = filedialog.askopenfilename(initialdir=initial_dir)
+# file_path = (
+#     "/Users/alehs/Downloads/DOHMH_New_York_City_Restaurant_Inspection_Results.csv"
+# )
 
 # -------------------------- Processing the Data --------------------------
 
@@ -47,6 +47,32 @@ def most_A_grade():
             boro = borough
 
     print(f"Borough: {boro}\nA Grade Restaurants: {most}")
+
+
+def most_A_grade_chart():
+    img_path = Path(os.path.expanduser("~/Desktop")) / Path("pie_chart.pdf")
+
+    borough_df = []
+
+    for _, restaurants in boroughs.items():
+        temp_df = pd.DataFrame(restaurants)
+        borough_df.append(temp_df)
+
+    out_df = pd.concat(borough_df, ignore_index=True)
+    out_df = out_df.sort_values(by=["SCORE"]).groupby(["BORO"])
+
+    colors = ["olive green", "tomato red", "cyan", "pink", "yellow"]
+    explode = (0.01, 0.01, 0.01)
+
+    pie_chart = out_df.sum().plot(
+        kind="pie",
+        title="Number of A Grade Restaurant per Borough",
+        colors=colors,
+        explode=explode,
+        figsize=(5, 5),
+    )
+
+    pie_chart.get_figure().savefig(img_path)
 
 
 def rating_base_data():
@@ -80,16 +106,18 @@ quit = False
 
 options = {
     1: most_A_grade,
-    2: rating_base_data,
-    3: boroughs_restaurants,
+    2: most_A_grade_chart,
+    3: rating_base_data,
+    4: boroughs_restaurants,
 }
 
 while not quit:
     print("Main Menu".center(50, "-"))
     print("\nSelect and option by entering the number or character:\n")
     print("\t1: Get Borough with most grade A restaurants")
-    print("\t2: Export data of restaurant withing each borough base on rating")
-    print("\t3: Export data of best restaurants from each borough")
+    print("\t2: Get Borough with most grade A restaurants Pie Chart")
+    print("\t3: Export data of restaurant withing each borough base on rating")
+    print("\t4: Export data of best restaurants from each borough")
     print("\tq: Exit")
     choice = input("Enter Choice: ")
     if choice == "q":
