4268b564b274fff9a215b4bee4bf3a9b3b35b860
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-11-29 23:50:18 -0500

    Export data to a csv file base on ratings

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import  tkinter as tk;
+import tkinter as tk
+from pathlib import Path
 from tkinter import filedialog
 import os
 from pprint import pprint
@@ -13,7 +14,9 @@ from time import sleep
 
 # initial_dir = os.path.expanduser("~/Desktop")
 # file_path = filedialog.askopenfilename(initialdir=initial_dir)
-file_path = "/Users/alehs/Downloads/DOHMH_New_York_City_Restaurant_Inspection_Results.csv"
+file_path = (
+    "/Users/alehs/Downloads/DOHMH_New_York_City_Restaurant_Inspection_Results.csv"
+)
 
 # -------------------------- Processing the Data --------------------------
 
@@ -28,28 +31,48 @@ boroughs = resturant_data.get_boroughs()
 
 # -------------------------- Operations --------------------------
 
+
 def default_case():
     print("Wrong Choice, Please Try Again")
     sleep(1.5)
 
-def most_A_grade():
 
+def most_A_grade():
     boro = ""
     most = 0
 
     for borough in boroughs:
-        if( most <  len(boroughs[borough])):
+        if most < len(boroughs[borough]):
             most = len(boroughs[borough])
             boro = borough
 
     print(f"Borough: {boro}\nA Grade Restaurants: {most}")
 
+
 def rating_base_data():
-    print("")
+    csv_path = Path(os.path.expanduser("~/Desktop")) / Path("rating_data.csv")
+
+    borough_df = []
+
+    for _, restaurants in boroughs.items():
+        temp_df = pd.DataFrame(restaurants)
+        borough_df.append(temp_df)
+
+    out_df = pd.concat(borough_df, ignore_index=True)
+
+    out_df = out_df.sort_values(by=["SCORE"])
+
+    # out_df = out_df.reset_index(drop=True)
+
+    out_df.to_csv(csv_path, index=False)
+
+    print(
+        'CSV file save to desktop under the name "rating_data.csv"\nFile is sorted by Rating'
+    )
+
 
 def boroughs_restaurants():
     print()
-
 
 
 # -------------------------- Main Menu --------------------------
@@ -58,11 +81,10 @@ quit = False
 options = {
     1: most_A_grade,
     2: rating_base_data,
-    3: boroughs_restaurants,    
+    3: boroughs_restaurants,
 }
 
 while not quit:
-
     print("Main Menu".center(50, "-"))
     print("\nSelect and option by entering the number or character:\n")
     print("\t1: Get Borough with most grade A restaurants")
@@ -73,12 +95,9 @@ while not quit:
     if choice == "q":
         quit = True
         continue
-    
+
     print("\n")
-    print("-" * 50 )
+    print("-" * 50)
     options.get(int(choice), default_case)()
-    print("-" * 50 )
+    print("-" * 50)
     sleep(1)
-
-
-
--- restaurant_data.py
+++ restaurant_data.py
@@ -1,43 +1,50 @@
 import pandas as pd
 
+
 class RestaurantData:
     def __init__(self, file_path):
         # Load data from the specified CSV file
         self.data_frame = pd.read_csv(file_path)
 
         # Process the DataFrame to get boroughs and their restaurants based on A grade
-        self.boroughs ={
+        self.boroughs = {
             "BRONX": [],
             "BROOKLYN": [],
             "MANHATTAN": [],
             "QUEENS": [],
-            "STATEN ISLAND": []
+            "STATEN ISLAND": [],
         }
         self.sort_by_rating()
         self._process_data()
 
     def _process_data(self):
-        for _,record in self.data_frame[self.data_frame["GRADE"] == "A"].iterrows():
-            if record["BORO"] != "Missing":
+        camis_set = set()  # a set of camis/id to avoid restaurant repetition
 
-    # Storing relevant info about a restaurant in their respective borough
-                restaurant = {
-                    "ID": record["CAMIS"],
-                    "NAME": record["DBA"],
-                    "CUISINE": record["CUISINE DESCRIPTION"],
-                    "BUILDING": record["BUILDING"],
-                    "STREET": record["STREET"],
-                    "BORO": record["BORO"],
-                    "ZIPCODE": record["ZIPCODE"],
-                    "PHONE": record["PHONE"],
-                    "GRADE": record["GRADE"],
-                    "SCORE": record["SCORE"]
-                }
-        
-                self.boroughs[record["BORO"]].append(restaurant)
+        for _, record in self.data_frame[self.data_frame["GRADE"] == "A"].iterrows():
+            if record["BORO"] != "Missing":
+                camis = record["CAMIS"]
+                if camis not in camis_set:
+                    # Storing relevant info about a restaurant in their respective borough
+                    restaurant = {
+                        "ID": record["CAMIS"],
+                        "NAME": record["DBA"],
+                        "CUISINE": record["CUISINE DESCRIPTION"],
+                        "BUILDING": record["BUILDING"],
+                        "STREET": record["STREET"],
+                        "BORO": record["BORO"],
+                        "ZIPCODE": record["ZIPCODE"],
+                        "PHONE": record["PHONE"],
+                        "GRADE": record["GRADE"],
+                        "SCORE": record["SCORE"],
+                    }
+
+                    self.boroughs[record["BORO"]].append(restaurant)
+                    camis_set.add(camis)
 
     def sort_by_rating(self):
-        self.data_frame = self.data_frame.sort_values(by=["SCORE", "GRADE"], ascending=[True, True])
+        self.data_frame = self.data_frame.sort_values(
+            by=["SCORE", "GRADE"], ascending=[True, True]
+        )
 
     def get_dataframe(self):
         return self.data_frame
